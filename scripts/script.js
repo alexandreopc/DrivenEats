d86d42a191e86254d24ccb53a9cc8d94b59f5dff
Alexandre Costa <alexandreodpc@gmail.com>
2022-01-29 19:01:25 -0300

    correcao automatica

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function selecionarPrato(nomePrato) {
+    alert(nomePrato);
+  /*const selecionado = document.querySelector(".pratos .selecionado");
+  if(selecionado !== null) { 
+    selecionado.classList.remove("selecionado");
+  }
+  nomePrato.classList.add("selecionado");*/
+}
