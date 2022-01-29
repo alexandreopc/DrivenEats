b2b75387ecee95bbcdecd9b350c6d3e9ede8ad9a
Alexandre Costa <alexandreodpc@gmail.com>
2022-01-29 19:37:26 -0300

    inicio responsividade agora vai

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,7 @@
 function selecionarPrato(nomePrato) {
-    alert(nomePrato);
   /*const selecionado = document.querySelector(".pratos .selecionado");
   if(selecionado !== null) { 
-    selecionado.classList.remove("selecionado");
+    selecionado.classList.toggle("selecionado");
   }
-  nomePrato.classList.add("selecionado");*/
+  nomePrato.classList.toggle("selecionado");*/
 }
